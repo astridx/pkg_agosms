b3cd36499a1adae8ca80697fff2412fe420e2428
Guido De Gobbis <guido@degobbis.de>
2019-07-19 11:15:32 +0200

    Revert "Maximaler Zoomfaktor erhöht"
    This reverts commit 79a65c7e5dfc16f7b3419882e8813033752c5573.

--- src/media/plg_fields_agosmsaddressmarker/js/site-agosmsaddressmarker-openstreetmap.js
+++ src/media/plg_fields_agosmsaddressmarker/js/site-agosmsaddressmarker-openstreetmap.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 		var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 		var osmAttrib = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
-		var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});
+		var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});
 		window['map' + unique].addLayer(osm);
 	});
 	// For all maps [end]
