79a65c7e5dfc16f7b3419882e8813033752c5573
Guido De Gobbis <guido@degobbis.de>
2019-07-19 10:54:45 +0200

    Maximaler Zoomfaktor erhöht

--- src/media/plg_fields_agosmsaddressmarker/js/site-agosmsaddressmarker-mapbox.js
+++ src/media/plg_fields_agosmsaddressmarker/js/site-agosmsaddressmarker-mapbox.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
 		var osmAttrib = 'Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, ' +
 			'<a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, ' +
 			'Imagery © <a href=\"http://mapbox.com\">Mapbox</a>';
-		var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib, id: 'mapbox.streets'});
+		var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib, id: 'mapbox.streets'});
 		window['map' + unique].addLayer(osm);
 	});
 	// For all maps [end]
