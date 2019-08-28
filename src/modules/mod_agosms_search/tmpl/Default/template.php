0288521d9a20c6e4208c63787b9d1d04b5a5a204
astridx <info@astrid-guenther.de>
2019-08-28 15:06:20 +0200

    js move

--- src/modules/mod_agosms_search/tmpl/Default/template.php
+++ src/modules/mod_agosms_search/tmpl/Default/template.php
@@ -28,7 +28,54 @@ $moduleclass_sfx = $params->get('moduleclass_sfx', '');
 ?>
 
 <script type="text/javascript">
-	<?php require(JModuleHelper::getLayoutPath('mod_agosms_search', $params->get('module_template', 'Default') . '/elements/basic_scripts')); ?>
+
+	var sliderLock = 0; // used for apply form change only after slider stop
+
+	jQuery(document).ready(function($) {
+		$("#GSearch<?php echo $module->id; ?> .gsearch-field-calendar").each(function() {
+			$(this).find("input:eq(0)").attr("placeholder", "<?php echo JText::_('MOD_AGOSMSSEARCHFILTER_TYPE_DATE_FROM'); ?>");
+			$(this).find("input:eq(1)").attr("placeholder", "<?php echo JText::_('MOD_AGOSMSSEARCHFILTER_TYPE_DATE_TO'); ?>");
+		});
+		$("#GSearch<?php echo $module->id; ?> .gsearch-field-calendar.single input").attr("placeholder", "<?php echo JText::_('MOD_AGOSMSSEARCHFILTER_TYPE_DATE'); ?>");
+		$(".gsearch-field-calendar input").addClass("inputbox");
+		
+		$("#GSearch<?php echo $module->id; ?> .gsearch-field-text input[name=keyword]").on("keyup", function(event) {
+			if(event.which == 13) {
+				submit_form_<?php echo $module->id; ?>();
+			}
+		});
+	
+		$("#GSearch<?php echo $module->id; ?> form").submit(function() {
+			<?php if($params->get("search_history")) { ?>	
+			search_history<?php echo $module->id; ?>();
+			<?php } ?>
+			<?php if($params->get("search_stats")) { ?>	
+			search_stats<?php echo $module->id; ?>();
+			<?php } ?>
+			$(".filter_loading<?php echo $module->id; ?>").show();
+			$(this).find(".inputbox, input[type=hidden]").each(function() {
+				if($(this).val() == '') {
+					$(this).attr("name", "");
+				}
+			});
+			setTimeout(function() {
+				$(".filter_loading<?php echo $module->id; ?>").hide();
+			}, 4000);
+		});
+		
+		$("body").append("<div class='filter_loading<?php echo $module->id; ?>'>loading ...</div>");
+	});
+	
+	function submit_form_<?php echo $module->id; ?>() {
+		if(sliderLock) return false;
+		<?php if($params->get("search_type") == "ajax") { ?>	
+		ajax_results<?php echo $module->id; ?>();
+		return false;
+		<?php } ?>
+		jQuery("#GSearch<?php echo $module->id; ?> form").submit(); 
+	}
+
+
 </script>
 
 <?php 
