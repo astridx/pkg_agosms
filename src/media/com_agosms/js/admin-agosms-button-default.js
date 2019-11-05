c6f813589ac57b2e6ca58e1e9d6dea592b6c9cd1
astridx <info@astrid-guenther.de>
2019-11-05 10:00:18 +0100

    js

--- src/media/com_agosms/js/admin-agosms-button-default.js
+++ src/media/com_agosms/js/admin-agosms-button-default.js
@@ -1,7 +1,3 @@
-/**
- * @copyright  Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
 (function () {
 	"use strict";
 	document.addEventListener('DOMContentLoaded', function () {
