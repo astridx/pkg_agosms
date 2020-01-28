e1148621356672845141860a995774f6ef9bcb91
astridx <info@astrid-guenther.de>
2020-01-28 19:27:34 +0100

    packagist and bug in ie with startswith

--- src/media/mod_agosm/js/agosm.js
+++ src/media/mod_agosm/js/agosm.js
@@ -607,6 +607,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
 		}
 
+		if (!String.prototype.startsWith) {
+			   String.prototype.startsWith = function(searchString, position){
+				 position = position || 0;
+				 return this.substr(position, searchString.length) === searchString;
+			 };
+		}
 		// Show Pins from customfield
 		if (showcustomfieldpin === '1')
 		{
