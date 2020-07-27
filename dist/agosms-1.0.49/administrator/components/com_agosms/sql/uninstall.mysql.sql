15a1ba9b09b7b679db97d0c19bb5ade8e9c9134b
Astrid <astridx@users.noreply.github.com>
2020-07-27 10:56:24 +0200

    Add Privacy Checkbox (#53)

--- dist/agosms-1.0.49/administrator/components/com_agosms/sql/uninstall.mysql.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM `#__content_types` WHERE `type_alias` IN ('com_agosms.agosm', 'com_agosms.category');
-
-DROP TABLE IF EXISTS `#__agosms`;
--- sik/media/media_20191104/plg_fields_agosmsaddressmarker/js/site-agosmsaddressmarker-mapbox.js
+++
@@ -1,6 +0,0 @@
-;
-document.addEventListener('DOMContentLoaded',function(){var e=document.querySelectorAll('.agosmsaddressmarkerleafletmap');[].forEach.call(e,function(a){var e=a.getAttribute('data-unique'),n=a.getAttribute('data-scrollwheelzoom'),p=a.getAttribute('data-mapboxkey'),m=L.DomUtil.get('map'+e);
-if(!m.children.length>0){if(n==='0'){window['map'+e]=new L.Map('map'+e,{scrollWheelZoom:!1})}
-else{window['map'+e]=new L.Map('map'+e,{scrollWheelZoom:!0})}};
-var t='https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='+p,o='Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',r=new L.TileLayer(t,{attribution:o,id:'mapbox.streets'});
-window['map'+e].addLayer(r)})},!1);
