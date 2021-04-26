0e6eb74d5f886733dace6e61c0c0de42fdc24fa8
astridx <info@astrid-guenther.de>
2021-04-26 10:56:37 +0200

    j4

--- j4/pkg_agosms/src/media/mod_agosm/js/agosm.js
+++ j4/pkg_agosms/src/media/mod_agosm/js/agosm.js
@@ -442,6 +442,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 		// Special Pins
 		if (showpin === '1') {
+			var index = 0;
 			for (var specialpin in specialpins) {
 				// skip loop if the property is from prototype
 				if (!specialpins.hasOwnProperty(specialpin))
@@ -504,6 +505,50 @@ document.addEventListener('DOMContentLoaded', function () {
 						this.closePopup();
 					});
 				}
+
+				index++;
+				var clickmarkerlista = document.querySelector('.agmarkerlista_specialpin' + index);
+
+				if (clickmarkerlista) {
+					clickmarkerlista.addEventListener('click', function () {
+						window['mymap' + moduleId].setView(tempMarker.getLatLng());
+						tempMarker.openPopup();
+					});
+				}
+				window['mymap' + moduleId].on("moveend", function (event) { 
+					var bounds =  event.target.getBounds();
+					var indexhidemove = 0;
+					for (var specialpin in specialpins) {
+						indexhidemove++;
+						var hidemove = document.querySelector('.agmarkerlistli_specialpin' + indexhidemove);
+						if(hidemove){
+							var cords = L.latLng(specialpins[specialpin].latlonpin.split(",", 3));
+							if (!bounds.contains(cords)) {
+								hidemove.hidden = true;
+							} else {
+								hidemove.hidden = false;
+							}
+						}
+					}
+				});
+				window['mymap' + moduleId].on("zoomend", function (event) { 
+					var bounds =  event.target.getBounds();
+					var indexhidezoom = 0;
+					for (var specialpin in specialpins) {
+						indexhidezoom++;
+						var hidezoom = document.querySelector('.agmarkerlistli_specialpin' + indexhidezoom);
+						if(hidezoom){
+							var cords = L.latLng(specialpins[specialpin].latlonpin.split(",", 3));
+							if (!bounds.contains(cords)) {
+								hidezoom.hidden = true;
+							} else {
+								hidezoom.hidden = false;
+							}
+						}
+					}
+				});
+
+
 			}
 		}
 
@@ -631,6 +676,42 @@ document.addEventListener('DOMContentLoaded', function () {
 						this.closePopup();
 					});
 				}
+
+				var clickgmarkerlista = document.querySelector('.agmarkerlista_component' + obj.id);
+
+				if (clickgmarkerlista) {
+					clickgmarkerlista.addEventListener('click', function () {
+						window['mymap' + moduleId].setView(tempMarker.getLatLng());
+						tempMarker.openPopup();
+					});
+				}
+				window['mymap' + moduleId].on("moveend", function (event) { 
+					var bounds =  event.target.getBounds();
+					for (var specialcomponentpin in specialcomponentpins) {
+						if(specialcomponentpins[specialcomponentpin].id){
+							var cords = L.latLng(specialcomponentpins[specialcomponentpin].coordinates.split(",", 3));
+							if (!bounds.contains(cords)) {
+								document.querySelector('.agmarkerlistli_component' + specialcomponentpins[specialcomponentpin].id).hidden = true;
+							} else {
+								document.querySelector('.agmarkerlistli_component' + specialcomponentpins[specialcomponentpin].id).hidden = false;
+							}
+						}
+					}
+				});
+				window['mymap' + moduleId].on("zoomend", function (event) { 
+					var bounds =  event.target.getBounds();
+					for (var specialcomponentpin in specialcomponentpins) {
+						if(specialcomponentpins[specialcomponentpin].id){
+							var cords = L.latLng(specialcomponentpins[specialcomponentpin].coordinates.split(",", 3));
+							if (!bounds.contains(cords)) {
+								document.querySelector('.agmarkerlistli_component' + specialcomponentpins[specialcomponentpin].id).hidden = true;
+							} else {
+								document.querySelector('.agmarkerlistli_component' + specialcomponentpins[specialcomponentpin].id).hidden = false;
+							}
+						}
+					}
+				});
+	
 			}
 
 			if (JSON.parse(sessionStorage.getItem('mapState')) && savestate === "1") {
@@ -642,7 +723,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 		}
 
-		// Show Pins from component
+		// One Pin from component
 		if (showcomponentpinone === '1') {
 
 			var clustermarkers = L.markerClusterGroup({
@@ -873,6 +954,43 @@ document.addEventListener('DOMContentLoaded', function () {
 
 					let popuptext = "<a href=' " + url + " '> " + title + " </a>";
 					tempMarkercf.bindPopup(popuptext.replace(/<img src="images/g, '<img src="' + uriroot + 'images'));
+
+					var clickgmarkerlista = document.querySelector('.agmarkerlista' + objcf.id);
+
+					if (clickgmarkerlista) {
+						clickgmarkerlista.addEventListener('click', function () {
+							window['mymap' + moduleId].setView(tempMarkercf.getLatLng());
+							tempMarkercf.openPopup();
+						});
+					}
+					window['mymap' + moduleId].on("moveend", function (event) { 
+						var bounds =  event.target.getBounds();
+						for (var specialcustomfieldpin in specialcustomfieldpins) {
+							if(specialcustomfieldpins[specialcustomfieldpin].id){
+								var cords = L.latLng(specialcustomfieldpins[specialcustomfieldpin].cords.split(",").slice(0, 2));
+								if (!bounds.contains(cords)) {
+									document.querySelector('.agmarkerlistli' + specialcustomfieldpins[specialcustomfieldpin].id).hidden = true;
+								} else {
+									document.querySelector('.agmarkerlistli' + specialcustomfieldpins[specialcustomfieldpin].id).hidden = false;
+								}
+							}
+						}
+					});
+			
+					window['mymap' + moduleId].on("zoomend", function (event) { 
+						var bounds =  event.target.getBounds();
+						for (var specialcustomfieldpin in specialcustomfieldpins) {
+							if(specialcustomfieldpins[specialcustomfieldpin].id){
+								var cords = L.latLng(specialcustomfieldpins[specialcustomfieldpin].cords.split(",").slice(0, 2));
+								if (!bounds.contains(cords)) {
+									document.querySelector('.agmarkerlistli' + specialcustomfieldpins[specialcustomfieldpin].id).hidden = true;
+								} else {
+									document.querySelector('.agmarkerlistli' + specialcustomfieldpins[specialcustomfieldpin].id).hidden = false;
+								}
+							}
+						}
+					});
+			
 					tempMarkercf.addTo(clustermarkers);
 				}
 			}
