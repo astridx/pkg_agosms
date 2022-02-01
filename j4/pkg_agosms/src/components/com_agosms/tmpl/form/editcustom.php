440e86455c5966766572ccfe8fe2c9be9803a129
astridx <info@astrid-guenther.de>
2022-02-01 22:06:20 +0100

    saftyroadform

--- j4/pkg_agosms/src/components/com_agosms/tmpl/form/editcustom.php
+++ j4/pkg_agosms/src/components/com_agosms/tmpl/form/editcustom.php
@@ -3,140 +3,181 @@
  * @package     Joomla.Administrator
  * @subpackage  com_agosms
  *
- * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @copyright   (C) 2020 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-\defined('_JEXEC') or die;
+defined('_JEXEC') or die;
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Language\Multilanguage;
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Language\Associations;
-use Joomla\CMS\Router\Route;
+use Joomla\CMS\Language\Multilanguage;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Layout\LayoutHelper;
+use Joomla\CMS\Router\Route;
 
 HTMLHelper::_('behavior.keepalive');
 HTMLHelper::_('behavior.formvalidator');
-HTMLHelper::_('script', 'com_agosms/admin-agosms-letter.js', ['version' => 'auto', 'relative' => true]);
 
 $this->tab_name  = 'com-agosms-form';
 $this->ignore_fieldsets = ['details', 'item_associations', 'language'];
 $this->useCoreUI = true;
 ?>
-
-<script>
-window.addEventListener('load', (event) => {
-    document.getElementById("divagosmsstep2").style.display = "none";
-    document.getElementById("divagosmsstep3").style.display = "none";
-});
-
-function scrollToItem(item) {
-    var diff=(item.offsetTop-window.scrollY)/8
-    if (Math.abs(diff)>1) {
-        window.scrollTo(0, (window.scrollY+diff))
-        clearTimeout(window._TO)
-        window._TO=setTimeout(scrollToItem, 30, item)
-    } else {
-        window.scrollTo(0, item.offsetTop)
-    }
-}
-
-function step2() {
-    document.getElementById("agosmsstep2").style.display = "none";
-    document.getElementById("divagosmsstep2").style.display = "block";
-	scrollToItem(document.getElementById("heading2"));
-	//scrollToItem(document.getElementById("system-message-container));
-}
-
-function step3() {
-    document.getElementById("agosmsstep3").style.display = "none";
-    document.getElementById("divagosmsstep3").style.display = "block";
-    scrollToItem(document.getElementById("heading3"))
-}
-
-function step4() {
-    document.getElementById("agosmsstep3").style.display = "none";
-    document.getElementById("divagosmsstep3").style.display = "block";
-    scrollToItem(document.getElementById("heading3"))
-}
-</script>
-
-<h2>Submit an Event</h2>
-
-<form action="<?php echo Route::_('index.php?option=com_agosms&id=' . (int) $this->item->id); ?>" method="post"
-    name="adminForm" id="adminForm" class="form-validate form-vertical">
-    <div>
-
-        <h2>Title and Description</h2>
-        <?php echo Text::_('COM_AGOSMS_HINT_NAME'); ?>
-        <?php echo $this->form->renderField('name'); ?>
-
-        <?php echo $this->form->renderField('cusotm1'); ?>
-        <?php echo $this->form->renderField('cusotm2'); ?>
-        <?php echo $this->form->renderField('cusotm3'); ?>
-        <?php echo $this->form->renderField('cusotm4'); ?>
-        <?php echo $this->form->renderField('cusotm5'); ?>
-        <?php echo $this->form->renderField('cusotm6'); ?>
-        <?php echo $this->form->renderField('cusotm7'); ?>
-        <?php echo $this->form->renderField('cusotm8'); ?>
-        <?php echo $this->form->renderField('cusotm9'); ?>
-
-
-        <div class="mb-3 mt-4">
-            <button onclick="step2()" class="btn btn-primary w-100" id="agosmsstep2"><?php echo Text::_('Step 2'); ?>
-                <span class="icon-chevron-right" aria-hidden="true"></span>
-            </button>
-        </div>
+<div class="edit item-page<?php echo $this->pageclass_sfx; ?>">
+    <?php if ($this->params->get('show_page_heading')) : ?>
+    <div class="page-header">
+        <h1>
+            <?php echo $this->escape($this->params->get('page_heading')); ?>
+        </h1>
     </div>
+    <?php endif; ?>
 
 
 
+    <form action="<?php echo Route::_('index.php?option=com_agosms&id=' . (int) $this->item->id); ?>" method="post"
+        name="adminForm" id="adminForm" class="form-validate form-vertical">
+        <fieldset>
 
-    <div id="divagosmsstep2">
-        <hr>
-        <h2 id="heading2">Step 2 - Coordinate for Map</h2>
-        <?php echo Text::_('COM_AGOSMS_HINT_COORDINATES'); ?>
-        <?php echo $this->form->renderField('coordinates'); ?>
+            <?php echo $this->form->renderField('name'); ?>
+
+            <?php echo HTMLHelper::_('uitab.startTabSet', $this->tab_name, ['active' => 'details', 'recall' => true, 'breakpoint' => 768]); ?>
 
 
-        <div class="mb-3 mt-4">
-            <button onclick="step3()" class="btn btn-primary w-100" id="agosmsstep3"><?php echo Text::_('Step 3'); ?>
-                <span class="icon-chevron-right" aria-hidden="true"></span>
+            <?php echo HTMLHelper::_('uitab.addTab', $this->tab_name, 'cords', Text::_('COM_AGOSMS_FIELDSET_COORDS')); ?>
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cordsModal">
+			<?php echo Text::_('COM_AGOSMS_HINTS_AGOSM'); ?>
             </button>
-        </div>
 
-    </div>
+            <!-- Modal -->
+            <div class="modal fade" id="cordsModal" tabindex="-1" aria-labelledby="cordsModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="cordsModalLabel">
+                                <?php echo Text::_('COM_AGOSMS_FIELDSET_COORDS'); ?></h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <?php echo Text::_('COM_AGOSMS_HINT_COORDINATES'); ?>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php echo $this->form->renderField('coordinates'); ?>
+            <?php echo HTMLHelper::_('uitab.endTab'); ?>
 
-    <div id="divagosmsstep3">
-        <hr>
-        <h2 id="heading3">Step 3 - Futher informations</h2>
-        <?php echo Text::_('COM_AGOSMS_HINT_CUSTOM'); ?>
+			
+            <?php echo HTMLHelper::_('uitab.addTab', $this->tab_name, 'details', empty($this->item->id) ? Text::_('COM_AGOSMS_DESC_AGOSM') : Text::_('COM_AGOSMS_EDIT_AGOSM')); ?>
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#descModal">
+                <?php echo Text::_('COM_AGOSMS_HINTS_AGOSM'); ?>
+            </button>
 
-        <?php echo Text::_('COM_AGOSMS_HINT_DESCRIPTION'); ?>
-        <?php echo $this->form->renderField('description'); ?>
+            <!-- Modal -->
+            <div class="modal fade" id="descModal" tabindex="-1" aria-labelledby="descModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="descModalLabel"><?php echo Text::_('COM_AGOSMS_DESC_AGOSM'); ?>
+                            </h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <?php echo Text::_('COM_AGOSMS_HINT_DESCRIPTION'); ?>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php echo $this->form->renderField('description'); ?>
+            <?php echo HTMLHelper::_('uitab.endTab'); ?>
 
-        <div style="display:none">
-            <?php echo $this->form->renderField('popuptext'); ?>
-            <?php echo LayoutHelper::render('joomla.edit.params', $this); ?>
-        </div>
+            <?php echo HTMLHelper::_('uitab.addTab', $this->tab_name, 'custom', Text::_('COM_AGOSMS_FIELDSET_CUSTOM')); ?>
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#detailModal">
+			<?php echo Text::_('COM_AGOSMS_HINTS_AGOSM'); ?>
+            </button>
 
-        <input type="hidden" name="task" value="" />
-        <input type="hidden" name="return" value="<?php echo $this->return_page; ?>" />
-        <?php echo HTMLHelper::_('form.token'); ?>
+            <!-- Modal -->
+            <div class="modal fade" id="detailModal" tabindex="-1" aria-labelledby="detailModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="detailModalLabel">Further details on the event</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <?php echo Text::_('COM_AGOSMS_HINT_CUSTOM'); ?>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php echo $this->form->renderField('cusotm1'); ?>
+            <?php echo $this->form->renderField('cusotm2'); ?>
+            <?php echo $this->form->renderField('cusotm3'); ?>
+            <?php echo $this->form->renderField('cusotm4'); ?>
+            <?php echo $this->form->renderField('cusotm5'); ?>
+            <?php echo $this->form->renderField('cusotm6'); ?>
+            <?php echo $this->form->renderField('cusotm7'); ?>
+            <?php echo $this->form->renderField('cusotm8'); ?>
+            <?php echo $this->form->renderField('cusotm9'); ?>
+            <?php echo HTMLHelper::_('uitab.endTab'); ?>
 
 
-        <div class="mb-3 mt-4">
-            <button type="button" class="btn btn-primary w-100" onclick="scrollToItem(document.getElementById('system-message-container'));Joomla.submitbutton('agosm.save')">
-                <span class="fas fa-check" aria-hidden="true"></span>
+            <?php if (is_null($this->item->id)) : ?>
+            <?php echo $this->form->renderField('alias'); ?>
+            <?php endif; ?>
+
+            <?php echo $this->form->renderFieldset('details'); ?>
+            <?php echo HTMLHelper::_('uitab.endTab'); ?>
+
+            <div style="display:none">
+                <?php echo HTMLHelper::_('uitab.addTab', $this->tab_name, 'misc', Text::_('COM_AGOSMS_FIELDSET_MISCELLANEOUS')); ?>
+                <?php echo $this->form->getInput('misc'); ?>
+                <?php echo HTMLHelper::_('uitab.endTab'); ?>
+            </div>
+
+            <div style="display:none">
+                <?php if (Multilanguage::isEnabled()) : ?>
+                <?php echo HTMLHelper::_('uitab.addTab', $this->tab_name, 'language', Text::_('JFIELD_LANGUAGE_LABEL')); ?>
+                <?php echo $this->form->renderField('language'); ?>
+                <?php echo HTMLHelper::_('uitab.endTab'); ?>
+                <?php else: ?>
+                <?php echo $this->form->renderField('language'); ?>
+                <?php endif; ?>
+            </div>
+            <div style="display:none">
+                <?php echo $this->form->renderField('popuptext'); ?>
+                <?php echo LayoutHelper::render('joomla.edit.params', $this); ?>
+            </div>
+
+            <div style="display:none">
+                <?php echo LayoutHelper::render('joomla.edit.params', $this); ?>
+            </div>
+            <?php echo HTMLHelper::_('uitab.endTabSet'); ?>
+
+            <input type="hidden" name="task" value="" />
+            <input type="hidden" name="return" value="<?php echo $this->return_page; ?>" />
+            <?php echo HTMLHelper::_('form.token'); ?>
+        </fieldset>
+        <div class="mb-2">
+            <button type="button" class="btn btn-primary" onclick="window.scrollTo(0, 0);Joomla.submitbutton('agosm.save')">
+                <span class="icon-check" aria-hidden="true"></span>
                 <?php echo Text::_('JSAVE'); ?>
             </button>
-            <hr>
-            <button type="button" class="btn btn-danger w-100" onclick="Joomla.submitbutton('agosm.cancel')">
-                <span class="fas fa-times-cancel" aria-hidden="true"></span>
+            <button type="button" class="btn btn-danger" onclick="Joomla.submitbutton('agosm.cancel')">
+                <span class="icon-times" aria-hidden="true"></span>
                 <?php echo Text::_('JCANCEL'); ?>
             </button>
         </div>
-    </div>
-</form>
+    </form>
+</div>
